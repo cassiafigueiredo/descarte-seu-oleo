52d1d6c8a67acd13bf422445793c131474c05a6e
cassiafigueiredo <figueiredo.cassia05@gmail.com>
2025-03-15 16:20:53 -0300

    inserção de biblioteca route, home finalizada, aguardando rota para dicas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/Home/Home'
 import Dicas from './components/Dicas/Dicas'
 import Agenda from './components/Agenda/Agenda'
@@ -9,8 +10,12 @@ function App() {
 
   return (
     <>
-     <Home/>
-     <Dicas/>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dicas" element={<Dicas />} />
+      </Routes>
+    </Router>
      <Agenda/>
      <Informativo/>
     </>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,37 +1,49 @@
 import styles from "../Home/Home.module.css"
-
+import { Link } from "react-router-dom"
 
 function Home() {
     return (
         <>
         <div className={styles.backgroundHome}>
-        <nav className="navbar navbar-expand-lg d-flex justify-content-end">
-                <div className="container-fluid">
+        <nav className="navbar navbar-expand-lg d-flex justify-content-end me-3">
+                <div className="container-fluid d-flex justify-content-end">
                     
                      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                        </svg>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
                         <ul className="navbar-nav ms-auto">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <a className="nav-link active" aria-current="page" href="#">home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Features</a>
+                                <a className="nav-link" href="#">dicas</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Pricing</a>
+                                <a className="nav-link" href="#">agenda</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">causa</a>
                             </li>
                             
                         </ul>
                     </div>
                 </div>
         </nav>
-                <div className="h-100 d-flex justify-content-center align-items-center">
+                <div className="h-100 d-flex flex-column justify-content-center align-items-center">
                     <h1 className={styles.h1Home}>
                         Descarte seu óleo de forma correta e consciente.
                     </h1>
+                    <div className="d-flex justify-content-center mt-4">
+                    <Link to="/dicas#sessao-dicas" className={styles.botaoConhecer}>
+                    Conhecer!
+                    </Link>
+                    </div>
                 </div>
+                
+                
         </div>
         </>
     )
